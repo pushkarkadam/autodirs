6faf45432bbb9018c9bedec873498e8179e15eda
Pushkar Kadam <pushkarkadam17@outlook.com>
2020-09-25 15:52:13 +1000

    Create a text file in the new directory added.

--- examples/characters_dir.py
+++ examples/characters_dir.py
@@ -4,4 +4,4 @@ sys.path.append('../')
 from autodirs import directories as di
 
 
-di.create_directories_from_text("text_files/gryffindor.txt", "sub/house")
+di.create_directories_from_text("text_files/gryffindor.txt", "sub/house", with_text=True)
--- tests/test_directories.py
+++ tests/test_directories.py
@@ -7,7 +7,7 @@ sys.path.append('../')
 from autodirs import directories as di
 
 def test_create_directories_from_text():
-    di.create_directories_from_text("tests/test_files.txt", "tests/test_dir/test_create_directories")
+    di.create_directories_from_text("tests/test_files.txt", "tests/test_dir/test_create_directories", with_text=True)
 
     file_path = [dir.path for dir in os.scandir("tests/test_dir/test_create_directories/") if dir.is_dir()]
     test_file_path = [
@@ -20,7 +20,7 @@ def test_create_directories_from_text():
 
 
 def test_group_by_text_files():
-    di.group_by_text_files("tests", "tests/test_dir/test_group_by_text_files")
+    di.group_by_text_files("tests", "tests/test_dir/test_group_by_text_files", with_text=True)
 
     file_path = [dir.path for dir in os.scandir("tests/test_dir/test_group_by_text_files/test_files/") if dir.is_dir()]
 
@@ -40,7 +40,7 @@ def test_create_directories_from_list():
     "THE GREY LADY"
     ]
 
-    di.create_directories_from_list(ravenclaw, "tests/test_dir/test_create_directories_from_list")
+    di.create_directories_from_list(ravenclaw, "tests/test_dir/test_create_directories_from_list", with_text=True)
 
     file_path = [dir.path for dir in os.scandir("tests/test_dir/test_create_directories_from_list/") if dir.is_dir()]
 
